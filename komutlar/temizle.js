0224368a0bcfeebf7827d0b368f6d7939aea78cd
Glitch (salan) <none>
2019-11-24 09:06:22 +0000

    🎳📠 Checkpoint ./komutlar/temizle.js:3054458/1342

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,22 +1,22 @@
-const Discord = require("discord.js");
-
-
-module.exports.run = async (bot, message, args) => {
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu komutu kullanmak için izniniz yoktur!");
-if(!args[0]) return message.channel.send("**Lütfen silinecek mesaj adetini belirtin.**");
+//Code Hure Dolunay
+const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
+if(!args[0]) return message.channel.send("🚫 **Lütfen Silinicek Mesaj Miktarını Yazın.!** 🚫");
 message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(` ${args[0]} Adet mesajı sildim. ✅`).then(msg => msg.delete(5000));
+  message.channel.send(` ${args[0]} Adet Mesajı Sildim. ✅`).then(msg => msg.delete(5000));
 })
 }
+
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: [],
-  permLevel: 3
+  aliases: ['sil,t,s'],
+  permLevel: 2
 };
 
 exports.help = {
   name: 'temizle',
-  description: 'İstediğiniz kişinin banını kaldırır.',
-  usage: 'temizle'
+  description: 'Belirlenen miktarda mesajı siler.',
+  usage: 'temizle <silinicek mesaj sayısı>'
 };
