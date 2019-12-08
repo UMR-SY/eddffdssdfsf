d17325b03b737edc30c6c15d0bcfb9a0fcd49cde
Glitch (maximusboys-yeniii) <none>
2019-12-08 10:06:05 +0000

    Rewound to commit ff4b591c3ce4dc5bf3be92d69046096aed6b20f6 + 0 edits

--- bot.js
+++ bot.js
@@ -1626,7 +1626,7 @@ client.on("guildMemberAdd", async member => {
       ? gmesaj
           .replace("{kullanıcı}", `${member.user}`)
           .replace("{sunucu}", `${member.guild.name}`)
-      : `\`${member.user.tag}\`<a:check_yes:647092902626983946>  Adlı Kullanıcı \`${member.guild.name}\` Adlı Sunucuya Katıldı. (\`${prefix}giriş-mesaj\` komutu ile değiştirilebilir.)`
+      : `\`${member.user.tag}\` Adlı Kullanıcı \`${member.guild.name}\` Adlı Sunucuya Katıldı. (\`${prefix}giriş-mesaj\` komutu ile değiştirilebilir.)`
   );
 });
 
