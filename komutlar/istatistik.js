aea7cfdceb03cf875989352fbb81fd0bd7247eee
Glitch (maximusboys-yeniii) <none>
2019-12-08 10:41:49 +0000

    🍶👔 Checkpoint ./komutlar/istatistik.js:3054458/72

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -7,9 +7,9 @@ exports.run = async (client, message, params, args) => {
 
   const yardım = new Discord.RichEmbed()
   .setColor(0x36393E)
-      .setAuthor(``, client.user.avatarURL)
+      .setAuthor(`Maximus Boys`, client.user.avatarURL)
       .setThumbnail(client.user.avatarURL)
-      .addField(`AloneBot - İstatistik`, `:small_orange_diamond: **Bellek kullanımı  :** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n:small_blue_diamond: **Kullanıcılar  :** ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\n:small_orange_diamond: **Sunucular  :** ${client.guilds.size.toLocaleString()}\n:small_blue_diamond: **Kanallar  :** ${client.channels.size.toLocaleString()}\n:small_orange_diamond: **Discord.js sürüm :** v${Discord.version}\n:small_blue_diamond: **Ping  :** ${Math.round(client.ping)} ms \n\n:cyclone: **Version  :** 0.0.5\n\n              **AloneBot ©**`)
+      .addField(`Maximus Boys İstatistik `, `:small_orange_diamond: **Bellek kullanımı  :** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n:small_blue_diamond: **Kullanıcılar  :** ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\n:small_orange_diamond: **Sunucular  :** ${client.guilds.size.toLocaleString()}\n:small_blue_diamond: **Kanallar  :** ${client.channels.size.toLocaleString()}\n:small_orange_diamond: **Discord.js sürüm :** v${Discord.version}\n:small_blue_diamond: **Ping  :** ${Math.round(client.ping)} ms \n\n:cyclone: **Version  :** 0.0.5\n\n              **Maximus Boys ©**`)
       .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
   return message.channel.sendEmbed(yardım);
 
