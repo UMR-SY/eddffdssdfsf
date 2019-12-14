0b82884ec4607601cf68af6db8e6d8596c77697e
Glitch (mb-yeni) <none>
2019-12-14 17:28:46 +0000

    Rewound to commit e791e8195692c07d673ce727ff16d4bb0614a118 + 0 edits

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
 console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
- client.user.setActivity("");
+ client.user.setActivity("online");
   var oyun = [ 
         "m!yardım", 
         "m!bilgi", 
@@ -17,7 +17,7 @@ console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.gu
         "m!radyo",
         "m!müzik",
         "7/24 Aktif.",
-        '123 Sunucu |Maximus Boys'
+        '124 Sunucu |Maximus Boys'
   
     ];
 
