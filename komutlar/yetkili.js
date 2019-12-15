f51f1f1bab31eeb217b81cfdf3e3c34c0b303896
Glitch (mb-yeni) <none>
2019-12-15 07:49:32 +0000

    👟🍿 Checkpoint ./komutlar/yetkili.js:1789560/616:3054458/9928 ./ayarlar.json:3054458/59

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -6,11 +6,10 @@ let botid = ('602024501710159882') //bu yere botun id'sini yapıştırın.
 
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
-        .setAuthor(`${client.user.username} `, client.user.avatarURL)
+       .setAuthor(`${client.user.username} `, client.user.avatarURL)
         .setColor('0x36393E')
         .setTitle(`${client.user.username} - Komutlar`)
-        .setDescription(`:small_orange_diamond: | **${ayarlar.prefix}kasaaç**CSGO Kasa Açarsınız .\n :small_orange_diamond: | **${ayarlar.prefix}aşkölç** Belirttiğiniz Kişiye Aşkınızı Gösterir. \n :small_orange_diamond: |  **${ayarlar.prefix}düello** Belirttiğiniz Kişiyle Düello Atarsınız.\n :small_orange_diamond: | **${ayarlar.prefix}roblox (kullanıcı-adınız)** Roblox Profilinize Bakarsınız.\n :small_orange_diamond: | **${ayarlar.prefix}mcskin (minecraft-adınız)** Minecraft Profilinizi Görürsünüz.\n` + `:small_orange_diamond: |
-        .setThumbnail(client.user.avatarURL)
+.setDescription(`:small_orange_diamond: | **${ayarlar.prefix}kasaaç**CSGO Kasa Açarsınız .\n :small_orange_diamond: | **${ayarlar.prefix}aşkölç** Belirttiğiniz Kişiye Aşkınızı Gösterir. \n :small_orange_diamond: |  **${ayarlar.prefix}düello** Belirttiğiniz Kişiyle Düello Atarsınız.\n :small_orange_diamond: | **${ayarlar.prefix}roblox (kullanıcı-adınız)** Roblox Profilinize Bakarsınız.\n :small_orange_diamond: | **${ayarlar.prefix}mcskin (minecraft-adınız)** Minecraft Profilinizi Görürsünüz.\n` + `:small_orange_diamond: \n`)        .setThumbnail(client.user.avatarURL)
         .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=602024501710159882&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/aEUfsU9) **|** [Web Sitesi](https://topbots-tr.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
     return message.channel.sendEmbed(embed);
