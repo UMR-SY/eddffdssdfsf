131f93f1402e8825b590e92ffe3dacdab3038d3b
Glitch (mb-yeni) <none>
2019-12-15 10:18:36 +0000

    🚍🏄 Checkpoint ./bot.js:3054458/1 ./komutlar/jsrol.js:3054458/637

---
+++ komutlar/jsrol.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+exports.run = function(client, message) {
+ 
+  var role = message.guild.roles.find(role => role.name === ""); // JS Rolünün Tam Isminin Yazin
+  if (message.member.roles.has(role.id)) return message.channel.send("⛔ Zaten bu role sahipsin :/")
+  message.member.addRole(role);
+  message.channel.send(`✅ JavaScript rolü başarıyla verildi :)`);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['discord.js','javascript'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'js',
+  description: 'JavaScript kanallarına erişim sağlar.',
+  usage: 'js'
+};
