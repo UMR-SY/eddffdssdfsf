83ca857313dab9b03d1b102ff185fd123151c80f
Glitch (mb-yeni) <none>
2019-12-15 08:20:34 +0000

    🚇🏌 Checkpoint ./komutlar/yetkili.js:3054458/1 ./komutlar/afk.js:3054458/854 ./komutlar/kullanıcı.js:3054458/3108

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -1,52 +1,31 @@
-const Discord = require("discord.js");
-const moment = require("moment");
-var green = process.env.NODE_DISABLE_COLORS ? '' : '\x1b[32m';
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-require("moment-duration-format");
+let botid = ('602024501710159882') //bu yere botun id'sini yapıştırın.
+//eğer botunuz dbl(discord bot list) de yoksa Bota Oy Ver (Vote) olmucaktır.
 
-exports.run = (client, msg) => {
-  const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-  msg.channel.sendCode("Yetkili Komutları:",`
-ᴍ!ᴋᴀʏɪᴛ-ᴋᴀɴᴀʟ-ᴀʏᴀʀʟᴀ: ᴋᴀʏɪᴛ-ᴋᴀɴᴀʟ-ᴀʏᴀʀʟᴀ #ᴋᴀɴᴀʟ ᴀʏᴀʀʟᴀʀ. 
-ᴍ!ᴋᴀɴᴀʟᴀʏᴀʀʟᴀ: ᴋᴀʏɪᴛ ᴏʟᴀɴ ᴋɪsɪɴɪɴ ʙɪʟᴅɪʀɪᴍɪɴɪɴ ᴋᴀɴᴀʟɪɴɪ ᴀʏᴀʀʟᴀʀ. 
-ᴍ!ᴏᴛᴏᴛᴀɢ: sᴜɴᴜᴄᴜʏᴀ ɢɪʀᴇɴ ᴋɪsɪʟᴇʀ ɪᴄɪɴ ᴏᴛᴏ ᴛᴀɢ ᴠᴇʀɪʀ. 
-ᴍ!ᴄɪᴋɪs-ᴍᴇsᴀᴊ-ᴀʏᴀʀʟᴀ: ᴄɪᴋɪs ᴍᴇsᴀᴊɪɴɪ ᴅᴇɢɪsᴛɪʀᴍᴇɴɪᴢɪ sᴀɢʟᴀʀ. 
-ᴍ!sᴀ-ᴀs: sᴀ-ᴀs ᴀʏᴀʀʟᴀʀ. 
-ᴍ!sᴀʏᴀᴄ: sᴀʏᴀᴄ <sᴀʏɪ> <#ᴋᴀɴᴀʟ> 
-ᴍ!ᴏᴛᴏʀᴏʟ: sᴜɴᴜᴄᴜʏᴀ ɢɪʀᴇɴʟᴇʀᴇ ᴠᴇʀɪʟᴇᴄᴇᴋ ᴏʟᴀɴ ᴏᴛᴏʀᴏʟᴜ ᴀʏᴀʀʟᴀʀ. 
-ᴍ!ᴋᴀᴘᴀᴛᴏᴛᴏʀᴏʟ: ᴏᴛᴏʀᴏʟ ᴋᴀᴘᴀᴛᴍᴀ ɪsᴇ ʏᴀʀᴀʀ. 
-ᴍ!ᴏʏʟᴀᴍᴀ-ᴋᴀɴᴀʟ: ᴏʏʟᴀᴍᴀ ᴋᴀɴᴀʟɪɴɪ sᴇᴄᴍᴇɴɪᴢᴇ ʏᴀʀᴀʀ. 
-ᴍ!ʙᴀsᴠᴜʀᴜ-ᴋᴀɴᴀʟ-ᴀʏᴀʀʟᴀ: ʏᴇᴛᴋɪ ʙᴀsᴠᴜʀᴜʟᴀʀɪɴɪɴ ɢᴏɴᴅᴇʀɪʟᴇᴄᴇɢɪ ᴋᴀɴᴀʟɪ ᴀʏᴀʀʟᴀʀ. 
-ᴍ!ᴅᴜʏᴜʀᴜ-ᴋᴀɴᴀʟ-ᴀʏᴀʀʟᴀ: ᴅᴜʏᴜʀᴜ ᴋᴀɴᴀʟɪɴɪ ʙᴇʟɪʀʟᴇʀ.
-ᴍ!ɢɪʀɪsᴍᴇsᴀᴊ: ɢɪʀɪs ᴍᴇsᴀᴊɪɴɪ ᴀʏᴀʀʟᴀʀ. (ᴋᴜʟʟᴀɴɪᴄɪ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-ᴋᴜʟʟᴀɴɪᴄɪ-", sᴜɴᴜᴄᴜ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-sᴜɴᴜᴄᴜ-" ʏᴀᴢɪɴɪᴢ. 
-ᴍ!ᴄɪᴋɪsᴍᴇsᴀᴊ: ᴄɪᴋɪs ᴍᴇsᴀᴊɪɴɪ ᴀʏᴀʀʟᴀʀ. (ᴋᴜʟʟᴀɴɪᴄɪ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-ᴋᴜʟʟᴀɴɪᴄɪ-", sᴜɴᴜᴄᴜ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-sᴜɴᴜᴄᴜ-" ʏᴀᴢɪɴɪᴢ. 
-ᴍ!ᴏᴢᴇʟ-ɢᴏʀᴜsᴜʀᴜᴢ-ᴀʏᴀʀʟᴀ: sᴜɴᴜᴄᴜᴅᴀɴ ᴀʏʀɪʟᴀɴ ᴋɪsɪʏᴇ ᴏᴢᴇʟᴅᴇɴ ɢᴏɴᴅᴇʀɪʟᴇᴄᴇᴋ ᴍᴇsᴀᴊɪ ᴀʏᴀʀʟᴀʀ. (ᴋᴜʟʟᴀɴɪᴄɪ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-ᴋᴜʟʟᴀɴɪᴄɪ-", sᴜɴᴜᴄᴜ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-sᴜɴᴜᴄᴜ-" ʏᴀᴢɪɴɪᴢ. 
-ᴍ!ᴏᴢᴇʟ-ʜᴏsɢᴇʟᴅɪɴ-ᴀʏᴀʀʟᴀ: sᴜɴᴜᴄᴜʏᴀ ɢɪʀᴇɴ ᴋɪsɪʏᴇ ᴏᴢᴇʟᴅᴇɴ ɢᴏɴᴅᴇʀɪʟᴇᴄᴇᴋ ᴍᴇsᴀᴊɪ ᴀʏᴀʀʟᴀʀ. (ᴋᴜʟʟᴀɴɪᴄɪ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-ᴋᴜʟʟᴀɴɪᴄɪ-", sᴜɴᴜᴄᴜ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "-sᴜɴᴜᴄᴜ-" ʏᴀᴢɪɴɪᴢ. 
-ᴍ!ᴍᴏᴅ-ʟᴏɢ-ᴀʏᴀʀʟᴀ: ᴍᴏᴅᴇʀᴀsʏᴏɴ ᴋᴀʏɪᴛʟᴀʀɪ ᴋᴀɴᴀʟɪɴɪ ᴀʏᴀʀʟᴀʀ. 
-ᴍ!ɢᴋᴀɴᴀʟ: ʀᴇsɪᴍʟɪ ʜɢ ᴋᴀɴᴀʟɪɴɪ ʙᴇʟɪʀʟᴇʀ. 
-ᴍ!ʙᴀɴ: ɪsᴛᴇᴅɪɢɪɴɪᴢ ᴋɪsɪʏɪ ʙᴀɴʟᴀʀ. 
-ᴍ!ᴅᴜʏᴜʀᴜ: ᴅᴜʏᴜʀᴜ sɪsᴛᴇᴍɪ. 
-ᴍ!ᴄᴇᴋɪʟɪs: ᴄᴇᴋɪʟɪs ʏᴀᴘᴀʀ. 
-ᴍ!ʟɪɴᴋ-ᴇɴɢᴇʟʟᴇ: ʟɪɴᴋ ᴇɴɢᴇʟʟᴇᴍᴇ sɪsᴛᴇᴍɪɴɪ ᴀᴄɪᴘ ᴋᴀᴘᴀᴛᴍᴀɴɪᴢɪ sᴀɢʟᴀʀ. 
-ᴍ!ᴄᴀᴘsʟᴏᴄᴋ-ᴇɴɢᴇʟʟᴇᴍᴇ: ᴄᴀᴘsʟᴏᴄᴋ ᴋᴜʟʟᴀɴɪᴍɪɴɪ ᴇɴɢᴇʟʟᴇʀ. 
-ᴍ!ᴏʏ-ᴋɪᴄᴋ: sᴜɴᴜᴄᴜᴅᴀ ᴏʏ ᴋɪᴄᴋ sɪsᴛᴇᴍɪ ɪʟᴇ ʙɪʀɪɴɪ sᴜɴᴜᴄᴜᴅᴀɴ ᴀᴛᴍᴀʏᴀ ɪsᴇ ʏᴀʀᴀʀ. 
-ᴍ!sᴜɴᴜᴄᴜ-ᴋᴜʀ: ʙᴜʟᴜɴᴜʟᴀɴ sᴜɴᴜᴄᴜ ɪᴄɪɴ ɢᴇʀᴇᴋʟɪ ᴋᴀɴᴀʟʟᴀʀɪ ᴏʟᴜsᴛᴜʀᴜʀ. 
-ᴍ!ʟᴏɢ-ᴀʏᴀʀʟᴀ: ʟᴏɢ ᴋᴀɴᴀʟɪ ᴀʏᴀʀʟᴀsɪɴɪᴢ. 
-ᴍ!ʀᴇᴋʟᴀᴍ-ᴛᴀʀᴀᴍᴀsɪ: ᴋᴜʟʟᴀɴɪᴄɪʟᴀʀɪɴ ᴏʏɴᴜʏᴏʀ ᴍᴇsᴀᴊɪɴᴅᴀᴋɪ ᴠᴇ ᴋᴜʟʟᴀɴɪᴄɪ ᴀᴅʟᴀʀɪɴᴅᴀᴋɪ ʀᴇᴋʟᴀᴍʟᴀʀɪ ᴛᴀʀᴀʀ. 
-ᴍ!ᴏᴛᴏʀᴏʟ-ᴍᴇsᴀᴊ-ᴀʏᴀʀʟᴀ: ᴏᴛᴏʀᴏʟ ᴍᴇsᴀᴊɪɴɪ ᴀʏᴀʀʟᴀʀ. (ᴋᴜʟʟᴀɴɪᴄɪ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "{ᴋᴜʟʟᴀɴɪᴄɪ}", ʀᴏʟ ɪsᴍɪɴɪɴ ɢᴇʟᴇᴄᴇɢɪ ʏᴇʀᴇ "{ʀᴏʟ}" ʏᴀᴢɪɴɪᴢ.
-
-`);
+exports.run = (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+       .setAuthor(`${client.user.username} `, client.user.avatarURL)
+        .setColor('0x36393E')
+        .setTitle(`${client.user.username} - Komutlar`)
+.setDescription(`:small_orange_diamond: | **${ayarlar.prefix}kasaaç** CSGO Kasa Açarsınız .\n :small_orange_diamond: | **${ayarlar.prefix}aşkölç** Belirttiğiniz Kişiye Aşkınızı Gösterir. \n :small_orange_diamond: |  **${ayarlar.prefix}düello** Belirttiğiniz Kişiyle Düello Atarsınız.\n :small_orange_diamond: | **${ayarlar.prefix}roblox (kullanıcı-adınız)** Roblox Profilinize Bakarsınız.\n :small_orange_diamond: | **${ayarlar.prefix}mcskin (minecraft-adınız)** Minecraft Profilinizi Görürsünüz.\n`)        .setThumbnail(client.user.avatarURL)
+        .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=602024501710159882&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/aEUfsU9) **|** [Web Sitesi](https://topbots-tr.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
+    return message.channel.sendEmbed(embed);
+  
+  
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['yetkili','yetkilis'],
-  permLevel: 0
+  aliases: [''],
+  permLevel: 0,
 };
 
 exports.help = {
-  name: 'yetkili',
-  description: 'Tüm komutları listeler. İsterseniz bir komut hakkında yardım eder..',
-  usage: 'yetkili'
+  name: 'oyun',
+  description: '',
+  usage: ''
 };
