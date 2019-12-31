df932310139d228b7ba2e0ab44b8f01e68fdad74
Glitch (mb-yeni) <none>
2019-12-31 13:55:09 +0000

    🌑😼 Checkpoint ./komutlar/phyton.js:3054458/107 ./komutlar/html.js:3054458/107

--- komutlar/phyton.js
+++ komutlar/phyton.js
@@ -5,7 +5,7 @@ exports.run = function(client, message) {
   var role = message.guild.roles.find(role => role.name === "🟢 | Phyton"); // JS Rolünün Tam Isminin Yazin
   if (message.member.roles.has(656537029634555914)) return message.channel.send("⛔ Zaten bu role sahipsin ")
   message.member.addRole(role);
-  message.channel.send(`✅ Phyton Rolü Başarıyla Verildi `);
+  message.channel.send(`✅ Phyton Rolü Başarıyla Verildi NOT: JavaScript 2 Adlı Rolü Almak İçin 1 İnvite Yapman Lazım Yaptıktan Sonra Otomatik Olarak Verilecektir! `);
 };
 
 exports.conf = {
