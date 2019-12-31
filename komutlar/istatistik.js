3b7223d247ae958d786dbf65df9b869ca05d6357
Glitch (mb-yeni) <none>
2019-12-31 16:06:07 +0000

    🍽🚟 Checkpoint ./komutlar/istatistik.js:3054458/2423

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,31 +1,26 @@
-const Discord = require('discord.js');
-const loglar = require('../loglar.json');
+const Discord = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
 
-var prefix = loglar.prefix;
-
-exports.run = async (client, message, params, args) => {
-
-  const yardım = new Discord.RichEmbed()
-  .setColor(0x36393E)
-      .setAuthor(`Maximus Boys`, client.user.avatarURL)
-      .setThumbnail(client.user.avatarURL)
-      .addField(`Maximus Boys İstatistik `, `:small_orange_diamond: **Bellek kullanımı  :** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n:small_blue_diamond: **Kullanıcılar  :** ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\n:small_orange_diamond: **Sunucular  :** ${client.guilds.size.toLocaleString()}\n:small_blue_diamond: **Kanallar  :** ${client.channels.size.toLocaleString()}\n:small_orange_diamond: **Discord.js sürüm :** v${Discord.version}\n:small_blue_diamond: **Ping  :** ${Math.round(client.ping)} ms \n\n:cyclone: **Version  :** 0.0.5\n\n              **Maximus Boys ©**`)
-      .setFooter(`${message.author.username} tarafından istendi.`, message.author.avatarURL)
-  return message.channel.sendEmbed(yardım);
-
+exports.run = (client, msg) => {
+  const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+  msg.channel.sendCode("asciidoc", `= İstatistikler =
+• Bellek kullanımı :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Kullanıcılar     :: ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}
+• Sunucular        :: ${client.guilds.size.toLocaleString()}
+• Kanallar         :: ${client.channels.size.toLocaleString()}
+• Ping             :: ${client.ping}`);
 };
 
-
-  
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ["istatiskik","i","info","bilgi","İ","İstatistik","İSTATİSTİK","İNFO","İnfo","Bilgi","BİLGİ","bot durum","Botdurum","botdurum","bi","Bi","Bİ"],
-    permLevel: 0
-  };
-  
-  exports.help = {
-    name: 'istatistik',
-    description: 'Botun istatistiklerini gösterir.',
-    usage: 'istatistik'
-  };
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bot durum', 'i', 'bi', 'istatistikler', 'kullanımlar', 'botdurum', 'bd', 'istatisik', 'stats', 'stat'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'istatistik',
+  description: 'Botun istatistik gösterir.',
+  usage: 'istatistik'
+};
