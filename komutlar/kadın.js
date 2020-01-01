3614e2c7ab78161147a4d602d8fd226e969a1952
Glitch (mb-yeni) <none>
2020-01-01 07:46:59 +0000

    🏗🌽 Checkpoint ./komutlar/kayıt-ol.js:3054458/1232 ./komutlar/kadın.js:3054458/1375 ./komutlar/erkek.js:3054458/201

---
+++ komutlar/kadın.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const db = require("quick.db")
+
+exports.run = async (client, message, args) => {
+
+if(!message.member.roles.has("655019027256705033")) return message.channel.send(`Bu komutu kullanabilmek için \`'KAYIT EDECEK YETKİLİ ROL İSİM'\` yetkisine sahip olmasınız.`);
+  let kullanıcı = message.mentions.users.first()
+  if (!kullanıcı) return message.channel.send('Kullanıcıyı Etiketlemelisin!')
+  let rol = message.mentions.roles.first()
+  let member = message.guild.member(kullanıcı)
+  member.addRole('661836926982029318')
+  member.removeRole('')
+  let embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .addField(`Kayıt işlemi başarılı`, `**Kayıt edilen kullanıcı :** ${kullanıcı} \n**Kayıt işleminde verilen rol :** \`'VERİLECEK ROL İSİM\``)
+  .setThumbnail(client.user.avatarURL)
+  .setFooter(`Komutu kullanan yetkili : ${message.author.username}`) 
+  return message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  kategori: "KULLANICI KOMUTLARI",
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'kadın',
+  description: "Sunucuya kaydolmaya ne dersin ?",
+  usage: 'kayıt isim yaş'
+}
