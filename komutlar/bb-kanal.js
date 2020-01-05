89b27a3f8046f469c63e21f978803341eb874713
Glitch (strengthened-closet) <none>
2020-01-05 19:00:55 +0000

    Rewound to commit 43847c5ef7b4bcb7e06886dcbdd6c83288caefb1 + 0 edits

--- komutlar/bb-kanal.js
+++ komutlar/bb-kanal.js
@@ -6,11 +6,11 @@ let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog1.json", "utf8"));
 var prefix = ayarlar.prefix;
 
 exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`<a:hayr:620546385371987978> Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
   
   let channel = message.mentions.channels.first()
     if (!channel) {
-        message.channel.send(`<a:hayr:620546385371987978> **Yalnış Kullanım**\n**Doğru Kullanım:** m!bb-kanal #kanal`)
+        message.channel.send(`:x: **Yalnış Kullanım**\n**Doğru Kullanım:** m!bb-kanal #kanal`)
         return
     }
     if(!gkanal[message.guild.id]){
@@ -21,21 +21,20 @@ if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`<a:hay
     fs.writeFile("./ayarlar/glog1.json", JSON.stringify(gkanal), (err) => {
         console.log(err)
     })
-  
-    message.channel.send(`<a:evet:620544866807578635> ${channel} Çıkış Mesaj Kanal ayarlandı.`)
-  
+     message.channel.send(`╔══════════════════════╗`)
+    message.channel.send(`║${channel} Çıkış Mesaj Kanal ayarlandı.`)
+    message.channel.send(`╚══════════════════════╝`)
 }
     
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: [''],
-    kategori: "yetkili",
+    aliases: ['bb-kanal'],
     permLevel: 2
 }
 
 exports.help = {
     name: 'bb-kanal',
     description: 'Çıkış Kanalını Ayarlar.',
-    usage: 's+bb-kanal <#kanal>'
+    usage: 'bb-kanal <#kanal>'
 }
--- komutlar/hg-kanal.js
+++ komutlar/hg-kanal.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js')
 const fs = require('fs');
 const ayarlar = require('../ayarlar.json');
-let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog.json", "utf8"));
+let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog1.json", "utf8"));
 
-var prefix = ayarlar.prefix; "m!"
+var prefix = ayarlar.prefix;
 
 exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`<a:onaylanmad:660744451760586762> Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
   
   let channel = message.mentions.channels.first()
     if (!channel) {
-        message.channel.send(`<a:onaylanmad:660744451760586762> **Yalnış Kullanım**\n**Doğru Kullanım:  **m!hg-kanal #kanal`)
+        message.channel.send(` **Yalnış Kullanım**\n**Doğru Kullanım:** m!bb-kanal #kanal`)
         return
     }
     if(!gkanal[message.guild.id]){
@@ -18,12 +18,12 @@ if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`<a:ona
             resim: channel.id
         };
     }
-    fs.writeFile("./ayarlar/glog.json", JSON.stringify(gkanal), (err) => {
+    fs.writeFile("./ayarlar/glog1.json", JSON.stringify(gkanal), (err) => {
         console.log(err)
     })
-
-    message.channel.send(`<a:onayland:660744428025282600> ${channel} Giriş Mesaj Kanal ayarlandı.`)
-
+  
+    message.channel.send(`<a:onayland:660744428025282600> ${channel} Çıkış Mesaj Kanal ayarlandı.`)
+  
 }
     
 exports.conf = {
@@ -35,7 +35,7 @@ exports.conf = {
 }
 
 exports.help = {
-    name: 'hg-kanal',
-    description: 'Giriş Çıkış Kanalını Ayarlar.',
-    usage: 'hg-kanal <#kanal>'
+    name: 'bb-kanal',
+    description: 'Çıkış Kanalını Ayarlar.',
+    usage: 's+bb-kanal <#kanal>'
 }
