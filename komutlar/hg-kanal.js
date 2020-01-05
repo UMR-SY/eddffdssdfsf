bfa7c8ac5dd897ef9105491f97d2f2f0e62487c6
Glitch (strengthened-closet) <none>
2020-01-05 10:01:52 +0000

    Rewound to commit c593473c93136d707a6386ecc0316cc28dfc64cc + 0 edits

--- komutlar/hg-kanal.js
+++ komutlar/hg-kanal.js
@@ -1,18 +1,16 @@
-MAXIMUS BOYS KOD
-   
 const Discord = require('discord.js')
 const fs = require('fs');
 const ayarlar = require('../ayarlar.json');
-let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog.json", "utf8"));
+let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog1.json", "utf8"));
 
-var prefix = ayarlar.prefix; "m!"
+var prefix = ayarlar.prefix;
 
 exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`<a:hayr:620546385371987978> Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
   
   let channel = message.mentions.channels.first()
     if (!channel) {
-        message.channel.send(`<a:hayr:620546385371987978> **Yalnış Kullanım**\n**Doğru Kullanım:  **m!hg-kanal #kanal`)
+        message.channel.send(` **Yalnış Kullanım**\n**Doğru Kullanım:** m!bb-kanal #kanal`)
         return
     }
     if(!gkanal[message.guild.id]){
@@ -20,12 +18,12 @@ if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`<a:hay
             resim: channel.id
         };
     }
-    fs.writeFile("./ayarlar/glog.json", JSON.stringify(gkanal), (err) => {
+    fs.writeFile("./ayarlar/glog1.json", JSON.stringify(gkanal), (err) => {
         console.log(err)
     })
-
-    message.channel.send(`<a:evet:620544866807578635> ${channel} Giriş Mesaj Kanal ayarlandı.`)
-
+  
+    message.channel.send(`<a:onayland:660744428025282600> ${channel} Çıkış Mesaj Kanal ayarlandı.`)
+  
 }
     
 exports.conf = {
@@ -37,7 +35,7 @@ exports.conf = {
 }
 
 exports.help = {
-    name: 'hg-kanal',
-    description: 'Giriş Çıkış Kanalını Ayarlar.',
-    usage: 'hg-kanal <#kanal>'
+    name: 'bb-kanal',
+    description: 'Çıkış Kanalını Ayarlar.',
+    usage: 's+bb-kanal <#kanal>'
 }
