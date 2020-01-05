ff9ced7202e82cc4a115fbf78cbad6378754caa6
Glitch (strengthened-closet) <none>
2020-01-05 19:14:10 +0000

    📍🏣 Checkpoint ./ayarlar/glog.json:3054458/6 ./bot.js:3054458/7165 ./komutlar/bb-kanal.js:3054458/2461 ./komutlar/hg-kanal.js:3054458/2419 ./ayarlar.json:3054458/118

--- komutlar/hg-kanal.js
+++ komutlar/hg-kanal.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js')
 const fs = require('fs');
 const ayarlar = require('../ayarlar.json');
-let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog1.json", "utf8"));
+let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog.json", "utf8"));
 
-var prefix = ayarlar.prefix;
+var prefix = ayarlar.prefix; "m!"
 
 exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`<a:hayr:620546385371987978> Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
   
   let channel = message.mentions.channels.first()
     if (!channel) {
-        message.channel.send(` **Yalnış Kullanım**\n**Doğru Kullanım:** m!bb-kanal #kanal`)
+        message.channel.send(`<a:hayr:620546385371987978> **Yalnış Kullanım**\n**Doğru Kullanım:  **m!hg-kanal #kanal`)
         return
     }
     if(!gkanal[message.guild.id]){
@@ -18,24 +18,24 @@ if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Kom
             resim: channel.id
         };
     }
-    fs.writeFile("./ayarlar/glog1.json", JSON.stringify(gkanal), (err) => {
+    fs.writeFile("./ayarlar/glog.json", JSON.stringify(gkanal), (err) => {
         console.log(err)
     })
-  
-    message.channel.send(`<a:onayland:660744428025282600> ${channel} Çıkış Mesaj Kanal ayarlandı.`)
-  
+
+    message.channel.send(`<a:evet:620544866807578635> ${channel} Giriş Mesaj Kanal ayarlandı.`)
+
 }
     
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['hg-kanal'],
+    aliases: [''],
     kategori: "yetkili",
     permLevel: 2
 }
 
 exports.help = {
-    name: 'bb-kanal',
-    description: 'Çıkış Kanalını Ayarlar.',
-    usage: 's+bb-kanal <#kanal>'
+    name: 'hg-kanal',
+    description: 'Giriş Çıkış Kanalını Ayarlar.',
+    usage: 'hg-kanal <#kanal>'
 }
