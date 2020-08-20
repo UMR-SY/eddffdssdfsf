a443d4b16e559d3f77f317e86785578339fe1ee8
Glitch (strengthened-closet) <none>
2020-08-20 10:35:58 +0000

    ⛰☃️ Checkpoint ./komutlar/yardım.js:3054458/1372:15929221/664 ./readme.md:3054458/69

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,11 +6,11 @@ const ayarlar = require('../ayarlar.json');
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         .setAuthor(`${client.user.username} `, client.user.avatarURL)
-        .setColor('0x36393E')
+        .setColor('#FF0000')
         .setTitle(`${client.user.username} - Komutlar`)
-        .setDescription("!avatar | Avatarınıza Bakarsınız! \n !ban @kullanıcı | Belirttiğiniz Kişiyi Banlarsınız! \n 
+        .setDescription("!avatar | Avatarınıza Bakarsınız! \n !ban @kullanıcı | Belirttiğiniz Kişiyi Banlarsınız! \n !banner-yazı | Yazdığınız Yazıyı Banner Olarak Atar. \n !capslock-engelleme | Capslock Kullanımını Engeller. \n !duyuru-kanal-ayarla | Duyuru Kanalını Belirler. \n !duyuru | Belirtilen Duyuru Kanalında Duyuru Yapar! \n !girişçıkış | (Kullanıcı isminin geleceği yere "-kullanıcı-", sunucu isminin geleceği yere "-sunucu-" yazınız.) \n !güvenlik | Güvenlik Sistemi. \n !temizle [Mesaj Sayısı] | Belirtilen Miktarda Mesaj Temizler \n !yasaklar | Sunucudan Yasaklananlar Listesini Gösterir!  \n !yavaş-mod [Süre] | Belirtilen Süre Kadar Yavaş Mod Uygular! \n !yetkilerim | Yetkilerinizi Gösterir! \n !özel-hoşgeldin-ayarla [mesaj] | Sunucuya Giren Kişilere Özelden Hoşgeldin Mesajını Gönderir!  \n !özel-görüşürüz-ayarla [mesaj] | Sunucudan Çıkan Kişilere Özelden Belirtilen Yazıyı Gönderir! \n ")
         .setThumbnail(client.user.avatarURL)
-        .addField(`» Linkler`, `[Bot Davet Linki](BOT DAVET LİNKİ!) **|** [Destek Sunucusu](https://discord.gg/aEUfsU9) **|** [Web Sitesi](https://topbots-tr.glitch.me/)`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
+        .addField("» Linkler`, `[Bot Davet Linki](BOT DAVET LİNKİ!) **|** [Destek Sunucusu](DESTEK SUNUCUSU) ")
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
         .setImage("https://cdn.discordapp.com/attachments/710753344154763264/745950722822111293/mb_yeni_logo.png")  
     return message.channel.sendEmbed(embed);
